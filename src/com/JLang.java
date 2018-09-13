ab12203f421fd8dbab5c82a04056af710fadef9c
Alexander Kozarev <al1102@yandex.ru>
2018-09-14 00:10:57 +0500

    little fix

--- src/com/JLang.java
+++ src/com/JLang.java
@@ -34,41 +34,39 @@ import java.util.*;
 public class JLang {
 
     public static void main(String[] args) throws IOException {
+        // 0. from file to map
 
-        /* 0. Выгрузить из файла. Возможно, используя scanner
-         * 0.1 Выгрузить в ArrayList
-         * 0.2 Перегрузить в Map
-         */
-        // Пункт 0.1 Выгрузить в ArrayList
+        // from file ArrayList
         File testFile = new File("test1.in");
         BufferedReader fin = new BufferedReader(new FileReader(testFile));
         String line;
         ArrayList<String> arrayList = new ArrayList<>();
         while ((line = fin.readLine()) != null) {
-            //System.out.println(line); //проверили, что мы берем из файла в список
+            //System.out.println(line); // check list
             arrayList.add(line);
         }
+        fin.close();
 
         int N = Integer.parseInt(arrayList.get(0));
-        //System.out.println(N);  //возможно, лучше через "return N;"
+        //System.out.println(N);  // mb, better "return N;"
         int M = Integer.parseInt(arrayList.get(N + 1));
         //System.out.println(M);
 
-        // Пункт 0.2 Перегрузить в Map
+        // from list to map
         Map <String, Integer> hashMap = new HashMap<>();
 
         for (int i = 1; i < N + 1; i++) {
-            //System.out.println(arrayList.get(i)); //увидели, что захватили нужный диапазон
+            //System.out.println(arrayList.get(i)); // check range
 
             String[] arrayL = arrayList.get(i).split(" ");
-            //System.out.println(arrayL[0]);    //проверили, что split хорошо отработал
+            //System.out.println(arrayL[0]);    // check split
             //System.out.println(arrayL[1]);
-            hashMap.put(arrayL[0], Integer.parseInt(arrayL[1]));    //засовываем получившиеся после split'a значения в map
+            hashMap.put(arrayL[0], Integer.parseInt(arrayL[1]));    // put values after split to the map
         }
-        //проверим, что у нас получилось в map:
+        // check map:
         //for (Map.Entry <String, Integer> pair : hashMap.entrySet()) System.out.println(pair.getKey() + " " + pair.getValue());
 
-        /*1. Сортировка (лямбда, можно через сортировку словаря или массивов объектов)
+        /*1. Sorting (lambda, but may use sort dict or massive of objects)
          * 1.1 Для одинакового количества отсортировать по алфавиту. Для сортировки попробовать sort().
          * 1.2 Отсортировать по количеству применений. -> важнее, сортировать в последнюю очередь.
          * 1.3 Выгрузить оставшиеся M слов. Каждое "слово" из M сравнивается с ключами из словаря (функция:"начинается с ..."),
@@ -76,13 +74,13 @@ public class JLang {
          */
         for (int i = N + 2; i < N + 2 + M; i++) {
             String a = arrayList.get(i);
-            //System.out.println(a); //проверили, что мы берем те значения из списка, какие нам надо (все М)
+            //System.out.println(a); // check values from list (all М)
 			
             hashMap
                     .entrySet()
                     .stream()
                     .filter(v -> v.getKey().startsWith(a))
-                    //.sorted(Map.Entry.comparingByKey())     // сначала по ключу, потом по значению
+                    //.sorted(Map.Entry.comparingByKey())     // first key, than value
                     .sorted(Map.Entry.<String, Integer> comparingByValue().reversed())
                     .limit(10)
                     .forEach(entry -> System.out.println(entry.getKey()));
